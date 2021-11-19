2cc8f0e8a6dab3c7f273c53a1d3cd972b1e8e680
Govern0r <mike.tromba@gmail.com>
2021-11-19 10:32:54 -0500

    install flamelab.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,9 @@ export default {
 				href:
 					'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap',
 			},
+    ],
+    script: [
+      { src: 'https://flamelab.io/flamelab.js' },
     ]
   },
   /*
