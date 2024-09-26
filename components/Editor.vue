26b9b8c95c4853a1a0f0d00cfeb18aad6da20629
Govern0r <mike.tromba@gmail.com>
2024-09-26 12:43:26 -0500

    refactor applyfox banner to it's own compoentn

--- components/Editor.vue
+++ components/Editor.vue
@@ -7,25 +7,7 @@
 
 		<div class="panels glass-initial cssPanel" :class="{ wasCopied }">
 			<div class="panel">
-				<a class="af-link" href="https://applyfox.ai" target="_blank">
-					<div class="applyfox-bg">
-						<div>
-							<span class="af-new">NEW</span>
-							<span class="af-text">
-								🔥 Auto-Apply to 100's of Jobs With AI 🔥
-							</span>
-						</div>
-						<div class="af-url">
-							👉
-							<span style="font-weight: 400; font-size: 14px"
-								>Click to visit:</span
-							>
-							<span style="text-decoration: underline"
-								>ApplyFox.ai</span
-							>
-						</div>
-					</div>
-				</a>
+				<ApplyFoxBanner />
 				<h1>Glassmorphism CSS Generator</h1>
 				<h2>Create a CSS Glass Effect</h2>
 				<div class="editor-setting">
@@ -267,50 +249,5 @@ textarea {
 .cssPanel.wasCopied:after {
 	opacity: 1;
 	pointer-events: all;
-}
-.applyfox-bg {
-	background-image: url('/applyfox-bg.png');
-	background-position: center; /* Center the image */
-	background-repeat: repeat; /* Do not repeat the image */
-	background-size: cover; /* Resize the background image to cover the entire container */
-	padding: 16px;
-	border-radius: 8px;
-	box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-	margin-bottom: 24px;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	transition: all 0.1s ease;
-	border: 4px solid rgba(255, 255, 255, 0.1);
-}
-.applyfox-bg:hover {
-	transform: scale(1.02);
-	border: 4px solid rgb(23, 0, 59);
-}
-.af-link {
-	position: relative;
-	font-size: 16px;
-	font-weight: bold;
-	color: white;
-	text-decoration: none;
-}
-.af-new {
-	background: #008325;
-	border: 1px solid #2aee62;
-	box-shadow: 0 0 10px #00ff48b6;
-	color: white;
-	font-size: 13px;
-	padding: 3px 5px;
-	border-radius: 4px;
-	margin-right: 8px;
-}
-.af-text {
-	margin-top: 8px;
-	font-size: 16px;
-	margin-top: 4px;
-}
-.af-url {
-	margin-top: 8px;
 }
 </style>
